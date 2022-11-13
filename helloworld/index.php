fd2bf640fea73b70c3337381e58667fc89953bb1
BigJeffTheChef <marleygpeter@gmail.com>
2022-11-13 17:34:31 +0000

    begun layout design

--- helloworld/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "Hello" . " world" . "<br>";
-var_dump($_FILES);
---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Moodr</title>
+  <link href="src/styles/styles.css" rel="stylesheet">
+</head>
+<body>
+  
+  <?php //include 'src/templates/header.php' ?>
+  <div id="header">
+  <div>
+    ICON
+  </div>
+  <div>
+    Add Entry
+  </div>
+  <div>
+    Visualise
+  </div>
+  <div>
+    Manage Moods
+  </div>
+  <div>
+    Login | Register
+  </div>
+</div>
+
+  <div id="body">
+    <?php //include 'src/templates/nav.php' ?>
+    <div>nav menu?</div>
+    <div>content?</div>
+  </div>
+  
+  <?php //include 'src/templates/footer.php' ?>
+  <div id="footer">
+    footer contents
+  </div>
+
+</body>
+</html>
