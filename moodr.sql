fdec8f89e2e4d1c86acf34f1851177b82ad21400
BigJeffTheChef <marleygpeter@gmail.com>
2022-11-24 11:20:34 +0000

    added a backup of db

---
+++ moodr.sql
@@ -0,0 +1,374 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 24, 2022 at 12:19 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `moodr`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `usp_Create_User` (IN `username` VARCHAR(255), IN `pw` VARCHAR(255))   BEGIN
+    INSERT INTO tbl_user (tbl_user.username, tbl_user.password)
+    VALUES (username, fn_1WayEncrypt(pw, NULL));
+END$$
+
+--
+-- Functions
+--
+CREATE DEFINER=`root`@`localhost` FUNCTION `fn_1WayEncrypt` (`value` VARCHAR(255), `providedSalt` VARCHAR(6)) RETURNS VARBINARY(255)  BEGIN
+   	DECLARE salt VARCHAR(255);
+	DECLARE saltedHash VARBINARY(255);
+   	DECLARE encryptedValue VARBINARY(255);
+
+    SET salt = IF(ISNULL(providedSalt), SUBSTRING(SHA1(RAND()), 1, 6), providedSalt);
+	SET saltedHash = SHA1(CONCAT(salt, `value`));
+	SET encryptedValue = CONCAT(salt, saltedHash);
+	RETURN encryptedValue;
+END$$
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `fn_Check_Password_Login` (`username` VARCHAR(255), `plainPassword` VARCHAR(255)) RETURNS VARBINARY(255)  BEGIN
+	DECLARE storedPassword VARBINARY(255);
+	DECLARE salt VARCHAR(255);
+    
+	SET storedPassword = (SELECT `password` FROM tbl_user WHERE tbl_user.username = username);
+    
+    SET salt = SUBSTRING(storedPassword, 1, 6);    
+    
+    RETURN IF(fn_1WayEncrypt(plainPassword, salt) = storedPassword, storedPassword, 0);
+END$$
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `fn_Check_Password_Reauth` (`username` VARCHAR(255), `encryptedPassword` VARBINARY(255)) RETURNS VARBINARY(255)  BEGIN
+	DECLARE storedPassword VARBINARY(255);
+	SET storedPassword = (SELECT `password` FROM tbl_user WHERE tbl_user.username = username);    
+    RETURN IF(encryptedPassword = storedPassword, 1, 0);
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_activity`
+--
+
+CREATE TABLE `tbl_activity` (
+  `activity_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `icon_image_id` int(11) NOT NULL,
+  `activity_group_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_activity_group`
+--
+
+CREATE TABLE `tbl_activity_group` (
+  `activity_group_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `icon_image_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_entry`
+--
+
+CREATE TABLE `tbl_entry` (
+  `entry_id` int(11) NOT NULL,
+  `notes` text NOT NULL,
+  `timestamp` int(11) NOT NULL DEFAULT current_timestamp(),
+  `activity_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_entry_image`
+--
+
+CREATE TABLE `tbl_entry_image` (
+  `entry_image_id` int(11) NOT NULL,
+  `entry_id` int(11) NOT NULL,
+  `image_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_entry_mood`
+--
+
+CREATE TABLE `tbl_entry_mood` (
+  `entry_mood_id` int(11) NOT NULL,
+  `entry_id` int(11) NOT NULL,
+  `mood_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_image`
+--
+
+CREATE TABLE `tbl_image` (
+  `image_id` int(11) NOT NULL,
+  `url` varchar(1000) NOT NULL,
+  `alt_text` varchar(255) NOT NULL,
+  `image_type_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_image_type`
+--
+
+CREATE TABLE `tbl_image_type` (
+  `image_type_id` int(11) NOT NULL,
+  `image_type_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_mood`
+--
+
+CREATE TABLE `tbl_mood` (
+  `mood_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `order` int(11) NOT NULL,
+  `icon_image_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varbinary(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`user_id`, `username`, `password`) VALUES
+(7, 'username', 0x30363463363365633335343764316366626231633734653965323531656637306434323462353364656631616139),
+(9, 'dax', 0x62633231363766643630396432653065316231316138626631383239616139386161666265653735346262613733),
+(12, 'django', 0x30626263613031383536663961306437613736653333393737343735346238383936626366623861626461333961);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_activity`
+--
+ALTER TABLE `tbl_activity`
+  ADD PRIMARY KEY (`activity_id`),
+  ADD KEY `FK__image__image_id_2` (`icon_image_id`),
+  ADD KEY `FK__activity_group__activity_group_id` (`activity_group_id`),
+  ADD KEY `FK__user__user_id_2` (`user_id`);
+
+--
+-- Indexes for table `tbl_activity_group`
+--
+ALTER TABLE `tbl_activity_group`
+  ADD PRIMARY KEY (`activity_group_id`),
+  ADD KEY `FK__image__image_id_3` (`icon_image_id`);
+
+--
+-- Indexes for table `tbl_entry`
+--
+ALTER TABLE `tbl_entry`
+  ADD PRIMARY KEY (`entry_id`),
+  ADD KEY `FK__activity__activity_id` (`activity_id`),
+  ADD KEY `FK__user__user_id_3` (`user_id`);
+
+--
+-- Indexes for table `tbl_entry_image`
+--
+ALTER TABLE `tbl_entry_image`
+  ADD PRIMARY KEY (`entry_image_id`),
+  ADD KEY `FK__image__image_id_4` (`image_id`),
+  ADD KEY `FK__entry__entry_id` (`entry_id`);
+
+--
+-- Indexes for table `tbl_entry_mood`
+--
+ALTER TABLE `tbl_entry_mood`
+  ADD PRIMARY KEY (`entry_mood_id`),
+  ADD UNIQUE KEY `entry_id` (`entry_id`,`mood_id`),
+  ADD KEY `FK__mood__mood_id_2` (`mood_id`);
+
+--
+-- Indexes for table `tbl_image`
+--
+ALTER TABLE `tbl_image`
+  ADD PRIMARY KEY (`image_id`),
+  ADD KEY `FK__image_type__image_type_id` (`image_type_id`);
+
+--
+-- Indexes for table `tbl_image_type`
+--
+ALTER TABLE `tbl_image_type`
+  ADD PRIMARY KEY (`image_type_id`);
+
+--
+-- Indexes for table `tbl_mood`
+--
+ALTER TABLE `tbl_mood`
+  ADD PRIMARY KEY (`mood_id`),
+  ADD UNIQUE KEY `order` (`order`),
+  ADD KEY `FK__image__image_id` (`icon_image_id`),
+  ADD KEY `FK__user__user_id` (`user_id`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_activity`
+--
+ALTER TABLE `tbl_activity`
+  MODIFY `activity_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_activity_group`
+--
+ALTER TABLE `tbl_activity_group`
+  MODIFY `activity_group_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_entry`
+--
+ALTER TABLE `tbl_entry`
+  MODIFY `entry_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_entry_image`
+--
+ALTER TABLE `tbl_entry_image`
+  MODIFY `entry_image_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_entry_mood`
+--
+ALTER TABLE `tbl_entry_mood`
+  MODIFY `entry_mood_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_image`
+--
+ALTER TABLE `tbl_image`
+  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_image_type`
+--
+ALTER TABLE `tbl_image_type`
+  MODIFY `image_type_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_mood`
+--
+ALTER TABLE `tbl_mood`
+  MODIFY `mood_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_activity`
+--
+ALTER TABLE `tbl_activity`
+  ADD CONSTRAINT `FK__activity_group__activity_group_id` FOREIGN KEY (`activity_group_id`) REFERENCES `tbl_activity_group` (`activity_group_id`),
+  ADD CONSTRAINT `FK__image__image_id_2` FOREIGN KEY (`icon_image_id`) REFERENCES `tbl_image` (`image_id`),
+  ADD CONSTRAINT `FK__user__user_id_2` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`);
+
+--
+-- Constraints for table `tbl_activity_group`
+--
+ALTER TABLE `tbl_activity_group`
+  ADD CONSTRAINT `FK__image__image_id_3` FOREIGN KEY (`icon_image_id`) REFERENCES `tbl_image` (`image_id`);
+
+--
+-- Constraints for table `tbl_entry`
+--
+ALTER TABLE `tbl_entry`
+  ADD CONSTRAINT `FK__activity__activity_id` FOREIGN KEY (`activity_id`) REFERENCES `tbl_activity` (`activity_id`),
+  ADD CONSTRAINT `FK__user__user_id_3` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`);
+
+--
+-- Constraints for table `tbl_entry_image`
+--
+ALTER TABLE `tbl_entry_image`
+  ADD CONSTRAINT `FK__entry__entry_id` FOREIGN KEY (`entry_id`) REFERENCES `tbl_entry` (`entry_id`),
+  ADD CONSTRAINT `FK__image__image_id_4` FOREIGN KEY (`image_id`) REFERENCES `tbl_image` (`image_id`);
+
+--
+-- Constraints for table `tbl_entry_mood`
+--
+ALTER TABLE `tbl_entry_mood`
+  ADD CONSTRAINT `FK__entry__entry_id_2` FOREIGN KEY (`entry_id`) REFERENCES `tbl_entry` (`entry_id`),
+  ADD CONSTRAINT `FK__mood__mood_id_2` FOREIGN KEY (`mood_id`) REFERENCES `tbl_mood` (`mood_id`);
+
+--
+-- Constraints for table `tbl_image`
+--
+ALTER TABLE `tbl_image`
+  ADD CONSTRAINT `FK__image_type__image_type_id` FOREIGN KEY (`image_type_id`) REFERENCES `tbl_image_type` (`image_type_id`);
+
+--
+-- Constraints for table `tbl_mood`
+--
+ALTER TABLE `tbl_mood`
+  ADD CONSTRAINT `FK__image__image_id` FOREIGN KEY (`icon_image_id`) REFERENCES `tbl_image` (`image_id`),
+  ADD CONSTRAINT `FK__user__user_id` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
