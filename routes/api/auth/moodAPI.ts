386a9c8fdefef5cf7d45cead6b4d9bfc15f3a7b0
BigJeffTheChef <marleygpeter@gmail.com>
2022-12-19 22:58:54 +0000

    entry endpoint dev ongoing

--- routes/api/auth/moodAPI.ts
+++ routes/api/auth/moodAPI.ts
@@ -7,6 +7,7 @@ import getConnection from '../../../lib/dbConnection';
 import PasswordQueryResponse from '../../../models/PasswordQueryResponse';
 import { encrypt } from '../../../lib/crypt';
 import { JwtPayload } from 'jsonwebtoken';
+import mysql from 'mysql2';
 import authenticateRequestSource from '../../../lib/authenticateRequestSource';
 
 const moodAPI = express.Router();
@@ -20,56 +21,111 @@ moodAPI.post('/entry', (req: Request, res: Response) => {
 	//entry activity(s)
 });
 
+const SQL = {
+	entry: {
+		entry:
+			`SELECT e.entry_id as entryId, e.notes as entryNotes FROM tbl_entry e
+		INNER JOIN tbl_mood m ON m.mood_id = e.mood_id
+		INNER JOIN tbl_mood_image mi ON mi.mood_image_id = m.icon_image_id
+		WHERE e.user_id = ?
+		LIMIT 10`,
+		activity:
+			`SELECT 
+			ea.entry_id,
+			a.name as activityName,
+			ai.url as activityIconUrl,
+			ai.alt_text as activityIconAltText,
+			ag.name as activityGroup,
+			agi.url as activityGroupIconUrl,
+			agi.alt_text as activityGroupIconAltText
+		FROM tbl_entry_activity ea 
+		INNER JOIN tbl_activity a ON ea.activity_id = a.activity_id
+		INNER JOIN tbl_activity_image ai ON a.icon_image_id = ai.activity_image_id
+		INNER JOIN tbl_activity_group ag ON a.activity_group_id = ag.activity_group_id
+		INNER JOIN tbl_activity_group_image agi ON agi.activity_group_image_id = ag.icon_image_id
+		WHERE ea.entry_id IN (?)`
+	}
+}
+
+
 moodAPI.get('/entry', async (req: Request, res: Response) => {
-	
+
 	let token;
 	try {
 		token = verifyToken(req.cookies.token);
 	} catch {
-		res.send('token verification failed!');
+		res.status(401).send('token verification failed!');
 		return;
 	}
-	
+
 	const con = await getConnection();
-	console.log(req.cookies.token);
-	const result = await con.execute('SELECT * FROM tbl_entry WHERE user_id=?', [94/*token*/]);
-	const results = result.at(0);
-	console.log(results);
+
+	// get entries from DB
+	let result = await con.execute(SQL.entry.entry, [94/*token*/]);
+	const entries = result.at(0) as Array<any>;
 	
-	/*
-	SELECT 
-e.entry_id AS entry_id,
-	e.timestamp AS `timestamp`,
-	m.name AS mood,
-	a.name AS activity,
-FROM tbl_entry e
-INNER JOIN tbl_mood AS m ON m.mood_id = e.mood_id
-INNER JOIN tbl_entry_activity AS ea ON ea.entry_id = e.entry_id
-INNER JOIN tbl_activity AS a ON ea.activity_id = a.activity_id
--- LEFT JOIN tbl_entry_images AS ei ON ei.entry_id = e.entry_id
-INNER JOIN tbl_activity_image AS ai ON ai.activity_image_id = a.icon_image_id
+	// get activities and data from db
+	result = await con.execute(mysql.format(SQL.entry.activity, [entries.map(e => e.entryId)]));
+	const activities = result.at(0) as Array<any>;
 
-WHERE e.user_id = (SELECT user_id FROM tbl_user WHERE tbl_user.username='randomcrap2');
+	// generate a map of entryid: entry data pairs
+	const entriesMap = new Map<number, entry>();
+	entries.forEach(e => {
+		entriesMap.set(e.entryId, {
+			entry_id: e.entryId,
+			entry_notes: e.entryNotes,
+			images: [],
+			activities: []
+		});
+	});
 
-SELECT * 
-FROM tbl_entry e
-INNER JOIN tbl_mood AS m ON m.mood_id = e.mood_id
-INNER JOIN tbl_entry_activity AS ea ON ea.entry_id = e.entry_id
-INNER JOIN tbl_activity AS a ON ea.activity_id = a.activity_id
--- LEFT JOIN tbl_entry_images AS ei ON ei.entry_id = e.entry_id
-WHERE e.user_id = (SELECT user_id FROM tbl_user WHERE tbl_user.username='randomcrap2');
-	*/
-
-
-	/* 
+	// console.log(activities);
 	
-	add entry
-	add entry images
-	add entry activities
-	
-	*/
+	// add all activities to entry objects
+	activities.forEach(e => {
+		let x = entriesMap.get(e.entry_id);
+		// console.log(x);
+		
+		x?.activities.push({
+			name: e.activityName,
+			icon: {
+				url: e.activityIconUrl,
+				altText: e.activityIconAltText
+			},
+			group: {
+				name: e.activityGroup,
+				icon: {
+					url: e.activityGroupIconUrl,
+					altText: e.activityGroupIconAltText
+				}
+			}
+		})
+	});
 
-	res.json(results);
+	res.json(Object.fromEntries(entriesMap));
 })
 
+
+interface entry {
+	entry_id: number,
+	entry_notes: string,
+	images: image[]
+	activities: activity[]
+}
+
+interface activity {
+	name: string,
+	icon: image,
+	group: activityGroup
+}
+
+interface activityGroup {
+	name: string,
+	icon: image
+}
+
+interface image {
+	url: string,
+	altText: string
+}
 export default moodAPI;
